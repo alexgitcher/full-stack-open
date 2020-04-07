6de21374c7877313fef82b30dc00b5aad9e2bd7b
Alexey Cherkasov <alcherkasov@gmail.com>
2020-04-07 20:05:39 +0500

    part 1 exercise 1.6 unicafe step 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,70 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.title} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  const part1 = props.parts[0];
-  const part2 = props.parts[1];
-  const part3 = props.parts[2];
-
-  return (
-    <div>
-      <Part title={part1.name} exercises={part1.exercises}/>
-      <Part title={part2.name} exercises={part2.exercises}/>
-      <Part title={part3.name} exercises={part3.exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const totalExercises = props.parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return (
-    <p>Number of exercises {totalExercises}</p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const setToGood = newVal => () => setGood(newVal);
+  const setToNeutral = newVal => () => setNeutral(newVal);
+  const setToBad = newVal => () => setBad(newVal);
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <h1>give feedback</h1>
+      <p>
+        <Button handleClick={setToGood(good + 1)} text="good"/>
+        <Button handleClick={setToNeutral(neutral + 1)} text="neutral"/>
+        <Button handleClick={setToBad(bad + 1)} text="bad"/>
+      </p>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root')
-);
-
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
